79d1359107f45908fad4244a0aba6228720c1e2b
tyabus <tyabustest@gmail.com>
2022-08-23 20:59:00 +0600

    Don't Error if failed to load platform module

--- game/client/cstrike15/gameui/vguisystemmoduleloader.cpp
+++ game/client/cstrike15/gameui/vguisystemmoduleloader.cpp
@@ -130,7 +130,7 @@ bool CVGuiSystemModuleLoader::LoadPlatformModules(CreateInterfaceFn *factorylist
 {
 #ifdef DEDICATED
 	return false;
-#endif
+#else
 
 	if ( IsGameConsole() )
 	{
@@ -182,7 +182,7 @@ bool CVGuiSystemModuleLoader::LoadPlatformModules(CreateInterfaceFn *factorylist
 		CSysModule *mod = g_pFullFileSystem->LoadModule(dllPath, "EXECUTABLE_PATH");
 		if (!mod)
 		{
-			Error("Platform Error: bad module '%s', not loading\n", it->GetString("dll"));
+			Warning("Platform Error: bad module '%s', not loading\n", it->GetString("dll"));
 			bSuccess = false;
 			continue;
 		}
@@ -205,6 +205,7 @@ bool CVGuiSystemModuleLoader::LoadPlatformModules(CreateInterfaceFn *factorylist
 
 	m_pPlatformModuleData = kv;
 	return InitializeAllModules(factorylist, factorycount) && bSuccess;
+#endif
 }
 
 //-----------------------------------------------------------------------------
