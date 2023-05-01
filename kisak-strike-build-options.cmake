9aaf018ed39b4dbcfdfc6e35404bc2a64df7de8b
tyabus <tyabustest@gmail.com>
2023-05-01 18:06:58 +0600

    Removed bullet physics, IVP is now compiled by default

--- kisak-strike-build-options.cmake
+++ kisak-strike-build-options.cmake
@@ -1,8 +1,6 @@
 # Welcome to the Kisak-Strike options file
 # Options here can be freely changed, however it is recommended to set the options via command line.
-# Example: cmake .. -DUSE_ROCKETUI=1 -DUSE_KISAK_PHYSICS=1 -DDEDICATED=0
-
-
+# Example: cmake .. -DUSE_ROCKETUI=1 -DDEDICATED=0
 
 # UI Options
 option(USE_ROCKETUI "Use New Open Source Kisak-Strike RmlUI" OFF)
@@ -12,15 +10,6 @@ option(USE_SCALEFORM "Use In-Complete Proprietary Flash UI with blob ( Not Recom
 # DEDICATED Server
 option(DEDICATED "Build as DEDICATED server. This is Separate from the main build and they are not in-tree compatible.
 Make sure to build with -DDEDICATED=0 once you want a regular client again." OFF)
-
-# Physics Options
-option(USE_KISAK_PHYSICS "Use the Open Source Physics Re-Build made for kisak-strike from various leaked sources" OFF)
-option(USE_BULLET_PHYSICS "Use Open Source Bullet3 Physics Engine(zlib)" OFF)
-option(USE_BULLET_PHYSICS_THREADED "Use Multi-Threading for the Bullet Physics Engine. Use convar 'bt_threadcount' to set." OFF)
-# 3rd option is to have both of these OFF, the closed source blob from Valve will be used instead.
-
-
-
 
 # Kisak-Strike Developer Options
 # (Gamer Tip: use gdb command `b __asan::ReportGenericError` to break on ASAN errors)
